959f5afd572e887a58dd771d39a0ed8654224007
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2017-02-12 19:03:53 -0200

    Gravando o primeio AUTOR .

--- bibliotecaReact/src/App.js
+++ bibliotecaReact/src/App.js
@@ -1,8 +1,61 @@
 import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
+import  $ from 'jquery';
 
 class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {lista : [],nome:'',email:'',senha:''};
+        this.enviaForm = this.enviaForm.bind(this);
+        this.setNome = this.setNome.bind(this);
+        this.setEmail = this.setEmail.bind(this);
+        this.setSenha = this.setSenha.bind(this);
+    }
+
+    componentDidMount(){
+        console.log("didMount");
+        $.ajax({
+                url:"http://localhost:8080/api/autores",
+                dataType: 'json',
+                success:function(resposta){
+                    console.log(resposta);
+                    console.log("chegou a resposta");
+                    this.setState({lista:resposta});
+                }.bind(this)
+            }
+        );
+    }
+    enviaForm(evento){
+        evento.preventDefault();
+        $.ajax({
+            url:'http://localhost:8080/api/autores',
+            contentType:'application/json',
+            dataType:'json',
+            type:'post',
+            data: JSON.stringify({nome:this.state.nome,email:this.state.email,senha:this.state.senha}),
+            success: function(resposta){
+                console.log("enviado com sucesso");
+            },
+            error: function(resposta){
+                console.log("erro");
+            }
+        });
+    }
+
+    setNome(evento){
+        this.setState({nome:evento.target.value});
+    }
+
+    setEmail(evento){
+        this.setState({email:evento.target.value});
+    }
+
+    setSenha(evento){
+        this.setState({senha:evento.target.value});
+    }
+
   render() {
     return (
         <div id="layout">
@@ -11,7 +64,6 @@ class App extends Component {
 
                 <span></span>
             </a>
-
             <div id="menu">
                 <div className="pure-menu">
                     <a className="pure-menu-heading" href="#">Company</a>
@@ -29,18 +81,18 @@ class App extends Component {
                 </div>
                 <div className="content" id="content">
                     <div className="pure-form pure-form-aligned">
-                        <form className="pure-form pure-form-aligned">
+                        <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm.bind(this)} method="post">
                             <div className="pure-control-group">
                                 <label htmlFor="nome">Nome</label>
-                                <input id="nome" type="text" name="nome" value=""  />
+                                <input id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome}/>
                             </div>
                             <div className="pure-control-group">
                                 <label htmlFor="email">Email</label>
-                                <input id="email" type="email" name="email" value=""  />
+                                <input id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail}/>
                             </div>
                             <div className="pure-control-group">
                                 <label htmlFor="senha">Senha</label>
-                                <input id="senha" type="password" name="senha"  />
+                                <input id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha}/>
                             </div>
                             <div className="pure-control-group">
                                 <label></label>
@@ -58,10 +110,16 @@ class App extends Component {
                             </tr>
                             </thead>
                             <tbody>
-                            <tr>
-                                <td>Hugo Henrique</td>
-                                <td>hugooliveira@gmail.com.br</td>
-                            </tr>
+                            {
+                                this.state.lista.map(function(autor){
+                                    return (
+                                       <tr>
+                                           <td>{autor.nome}</td>
+                                           <td>{autor.email}</td>
+                                       </tr>
+                                    );
+                                })
+                            }
                             </tbody>
                         </table>
                     </div>
