24eaa54f207b0f01c30f8e652da882b2fcd18c81
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2017-04-14 22:14:10 -0300

    Validacao dos campos .

--- bibliotecaReact/src/index.js
+++ bibliotecaReact/src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AutorBox from './Autor';
+import LivroBox from './Livro';
 import './index.css';
+import { BrowserRouter as Router, Route, browserHistory} from 'react-router-dom';
+
+
 
 ReactDOM.render(
-  <App />,
+  (<Router history={browserHistory}>
+    <div>
+        <Route exact path="/" component={App}/>
+        <Route path="/autor" component={AutorBox}/>
+        <Route path="/livro" component={LivroBox}/>
+    </div>
+  </Router>),
   document.getElementById('root')
 );
